c12304f64d51193d40d4c65959143607ad042964
Deepak Veliath <53190346+veliaD@users.noreply.github.com>
2019-02-13 16:22:40 +0000

    1) Do not register as a PCI device's driver 2) Shorten name prefix 3) Add linukpi as a dependency so dump_stack() can be used

--- nvmeorocev2/nvmeorocev2.c
+++ nvmeorocev2/nvmeorocev2.c
@@ -7,32 +7,14 @@
 #include <sys/module.h>
 #include <sys/systm.h>
 
-#include <dev/pci/pcireg.h>
-#include <dev/pci/pcivar.h>
+#include <linux/kernel.h>
 
 #include <rdma/ib_verbs.h>
 
-#define SPEWCMN(prefix, format, ...) printf(prefix "%s(%d) " format, \
+#define SPEWCMN(prefix, format, ...) printf(prefix "%s@%d> " format, \
     __func__, __LINE__, ## __VA_ARGS__)
 #define SPEW(format, ...) SPEWCMN("", format, ## __VA_ARGS__)
 #define ERRSPEW(format, ...) SPEWCMN("ERR|", format, ## __VA_ARGS__)
-
-struct nvmeorocev2_controller {
-	int field;
-};
-
-static device_method_t nvmeorocev2_pci_methods[] = {
-	/* Device interface */
-	{ 0, 0 }
-};
-
-static driver_t nvmeorocev2_pci_driver = {
-	"nvmeorocev2",
-	nvmeorocev2_pci_methods,
-	sizeof(struct nvmeorocev2_controller),
-};
-
-static devclass_t nvmeorocev2_devclass;
 
 /*
  * Invoked whenever the routine is registered with ib_register_client()
@@ -40,11 +22,12 @@ static devclass_t nvmeorocev2_devclass;
  * the routine is invoked for every IB interface already known.
  */
 static void
-nvmeorocev2_add_one(struct ib_device *ib_device)
+nrev2_add_ibif(struct ib_device *ib_device)
 {
-	SPEW("rdma_node_get_transport(%p)> %d\n", ib_device,
+	ERRSPEW("rdma_node_get_transport(%p)> %d\n", ib_device,
 	    rdma_node_get_transport(ib_device->node_type));
 }
+
 
 /*
  * Invoked whenever the routine is unregistered with ib_unregister_client()
@@ -52,20 +35,21 @@ nvmeorocev2_add_one(struct ib_device *ib_device)
  * the routine is invoked for every IB interface already known.
  */
 static void
-nvmeorocev2_remove_one(struct ib_device *ib_device, void *client_data)
+nrev2_remove_ibif(struct ib_device *ib_device, void *client_data)
 {
-	SPEW("%p removed\n", ib_device);
+	ERRSPEW("%p removed\n", ib_device);
 }
 
+
 static struct ib_client nvmeofrocev2 = {
-	.name   = "nvmeofrocev2_ib_client",
-	.add    = nvmeorocev2_add_one,
-	.remove = nvmeorocev2_remove_one
+	.name   = "nrev2_ib_client",
+	.add    = nrev2_add_ibif,
+	.remove = nrev2_remove_ibif
 };
 
 
 static void
-nvmeorocev2_load(void)
+nrev2_init(void)
 {
 	int retval;
 
@@ -76,41 +60,16 @@ nvmeorocev2_load(void)
 	}
 }
 
+
 static void
-nvmeorocev2_unload(void)
+nrev2_uninit(void)
 {
 	ib_unregister_client(&nvmeofrocev2);
-	return;
 }
 
 
-static void
-nvmeorocev2_unimplemented_modevent(void)
-{
-	return;
-}
-
-
-static int
-nvmeorocev2_modevent(module_t mod, int type, void *arg)
-{
-	ERRSPEW("modevent %d invoked\n", type);
-	switch (type) {
-	case MOD_LOAD:
-		nvmeorocev2_load();
-		break;
-	case MOD_UNLOAD:
-		nvmeorocev2_unload();
-		break;
-	default:
-		nvmeorocev2_unimplemented_modevent();
-		break;
-	}
-
-	return (0);
-}
-
-DRIVER_MODULE(nvmeorocev2, pci, nvmeorocev2_pci_driver, nvmeorocev2_devclass,
-    nvmeorocev2_modevent, 0);
-MODULE_DEPEND(nvmeorocev2, ibcore, 1, 1, 1);
-MODULE_VERSION(nvmeorocev2, 1);
+SYSINIT(nrev2, SI_SUB_DRIVERS, SI_ORDER_ANY, nrev2_init, NULL);
+SYSUNINIT(nrev2, SI_SUB_DRIVERS, SI_ORDER_ANY, nrev2_uninit, NULL);
+MODULE_DEPEND(nrev2, linuxkpi, 1, 1, 1);
+MODULE_DEPEND(nrev2, ibcore, 1, 1, 1);
+MODULE_VERSION(nrev2, 1);
